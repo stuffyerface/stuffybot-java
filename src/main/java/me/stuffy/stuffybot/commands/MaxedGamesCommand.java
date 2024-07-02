e49b4b745c0c8c1250f74771bd00320de33a7ec1
Stuffy <stuffinessmc@gmail.com>
2024-07-02 19:49:04 -0400

    tournament data

--- src/main/java/me/stuffy/stuffybot/commands/MaxedGamesCommand.java
+++ src/main/java/me/stuffy/stuffybot/commands/MaxedGamesCommand.java
@@ -4,21 +4,27 @@ import com.google.gson.JsonObject;
 import me.stuffy.stuffybot.profiles.HypixelProfile;
 import me.stuffy.stuffybot.profiles.MojangProfile;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.UUID;
 
 import static me.stuffy.stuffybot.utils.APIUtils.getHypixelProfile;
 import static me.stuffy.stuffybot.utils.APIUtils.getMojangProfile;
+import static me.stuffy.stuffybot.utils.DiscordUtils.getUsername;
 import static me.stuffy.stuffybot.utils.DiscordUtils.makeErrorEmbed;
 
 public class MaxedGamesCommand extends BaseCommand{
 
     public MaxedGamesCommand(String name, String description) {
-        super(name, description);
+        super(name, description,
+                new OptionData(OptionType.STRING, "ign", "Your Minecraft Username", false)
+        );
     }
     @Override
     protected void onCommand(SlashCommandInteractionEvent event) {
-        String ign = event.getOption("ign").getAsString(); // maybe a utility method to get the ign from the event
+        String ign = getUsername(event);
+
         MojangProfile profile;
         try {
             profile = getMojangProfile(ign);
@@ -34,6 +40,6 @@ public class MaxedGamesCommand extends BaseCommand{
         }
         UUID uuid = profile.getUuid();
         HypixelProfile hypixelProfile = getHypixelProfile(uuid);
-        hypixelProfile.getDiscord();
+        hypixelProfile.getMaxedGames();
     }
 }
