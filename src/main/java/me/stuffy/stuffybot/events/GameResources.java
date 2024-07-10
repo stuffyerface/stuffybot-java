227948f23d507ba5ee765e0c644531efbe005cc8
Stuffy <stuffinessmc@gmail.com>
2024-07-09 20:26:29 -0400

    remove events for now

--- src/main/java/me/stuffy/stuffybot/events/GameResources.java
+++
@@ -1,5 +0,0 @@
-package me.stuffy.stuffybot.events;
-
-public class GameResources {
-    // Check for changes in http://api.hypixel.net/resources/games
-}
--- src/main/java/me/stuffy/stuffybot/events/HypixelMOTDUpdate.java
+++
@@ -1,5 +0,0 @@
-package me.stuffy.stuffybot.events;
-
-public class HypixelMOTDUpdate {
-    // Check for changes in Hypixel Network's Server MOTD
-}
