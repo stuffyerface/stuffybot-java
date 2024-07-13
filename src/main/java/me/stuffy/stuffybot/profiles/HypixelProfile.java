5f0c0c26ec56499c70f54fbbd9e16da8bc971a45
Stuffy <stuffinessmc@gmail.com>
2024-07-13 08:44:59 -0400

    make stats return ints/doubles instead of strings

--- src/main/java/me/stuffy/stuffybot/profiles/HypixelProfile.java
+++ src/main/java/me/stuffy/stuffybot/profiles/HypixelProfile.java
@@ -65,14 +65,6 @@ public class HypixelProfile {
         return getNestedJson(profile, "socialMedia", "links", "DISCORD").getAsString();
     }
 
-    public JsonElement getAchievements() {
-        // return an object with profile["achievements"],profile["achievementsOneTime"]
-        JsonObject combined = new JsonObject();
-        combined.add("achievements", getNestedJson(profile, "achievements"));
-        combined.add("achievementsOneTime", getNestedJson(profile, "achievementsOneTime"));
-        return combined;
-    }
-
     public String[] getMaxedGames() {
         // return an array of strings with the maxed games
         JsonElement allAchievements = getAchievementsResources();
@@ -90,30 +82,27 @@ public class HypixelProfile {
         return discordTimeUnix(firstLogin, "D") + " " + discordTimeUnix(firstLogin, "R");
     }
 
-    public String getNetworkLevel() {
+    public Double getNetworkLevel() {
         if (!profile.has("networkExp")) {
-            return "0";
+            return 0.0;
         }
-        Integer networkExp = getNestedJson(profile, "networkExp").getAsInt();
-        DecimalFormat df = new DecimalFormat("#,###.##");
-        return df.format((Math.floor(Math.sqrt(networkExp + 15312.5) - 125/Math.sqrt(2)))/(25*Math.sqrt(2)));
+        int networkExp = getNestedJson(profile, "networkExp").getAsInt();
+        return (Math.floor(Math.sqrt(networkExp + 15312.5) - 125/Math.sqrt(2)))/(25*Math.sqrt(2));
     }
 
-    public String getKarma() {
+    public Integer getKarma() {
         if (!profile.has("karma")) {
-            return "0";
+            return 0;
         }
-        Integer karma = getNestedJson(profile, "karma").getAsInt();
-        DecimalFormat df = new DecimalFormat("#,###");
-        return df.format(karma);
+        return getNestedJson(profile, "karma").getAsInt();
     }
 
-    public String getAchievementPoints() {
+    public Integer getAchievementPoints() {
         if (!profile.has("achievementPoints")) {
-            return "0";
+            return 0;
         }
-        DecimalFormat df = new DecimalFormat("#,###");
-        return df.format(getNestedJson(profile, "achievementPoints").getAsInt());
+
+        return getNestedJson(profile, "achievementPoints").getAsInt();
     }
 
     public String getOnlineStatus() {
@@ -124,11 +113,11 @@ public class HypixelProfile {
         return status;
     }
 
-    public String getQuestsCompleted() {
+    public Integer getQuestsCompleted() {
         if (!profile.has("quests")) {
-            return "0";
+            return 0;
         }
-        Integer questsCompleted = 0;
+        int questsCompleted = 0;
         JsonElement quests = getNestedJson(profile, "quests");
         for (String key : quests.getAsJsonObject().keySet()) {
             if (quests.getAsJsonObject().get(key).getAsJsonObject().has("completions")) {
@@ -136,89 +125,237 @@ public class HypixelProfile {
             }
         }
 
-        DecimalFormat df = new DecimalFormat("#,###");
-        return df.format(questsCompleted);
+        return questsCompleted;
     }
 
-    public String getChallengesCompleted() {
+    public Integer getChallengesCompleted() {
         if (!profile.has("challenges")) {
-            return "0";
+            return 0;
         }
-        Integer challengesCompleted = 0;
+        int challengesCompleted = 0;
         JsonElement challenges = getNestedJson(profile, "challenges", "all_time");
         for (String key : challenges.getAsJsonObject().keySet()) {
             challengesCompleted += challenges.getAsJsonObject().get(key).getAsInt();
         }
 
-        DecimalFormat df = new DecimalFormat("#,###");
-        return df.format(challengesCompleted);
+        return challengesCompleted;
     }
 
-    public String getRewardStreak() {
-        String rewardStreak = "";
-        if (!profile.has("rewardScore")) {
-            rewardStreak += "0";
-        } else {
-            rewardStreak += getNestedJson(profile, "rewardScore").getAsString();
+    public Integer getRewardStreak() {
+        if (!profile.has("rewardStreak")) {
+            return 0;
         }
+        return getNestedJson(profile, "rewardStreak").getAsInt();
+    }
 
+    public Integer getRewardRecord() {
         if (!profile.has("rewardHighScore")) {
-            rewardStreak += "/0";
-        } else {
-            rewardStreak += "/" + getNestedJson(profile, "rewardHighScore").getAsString();
+            return 0;
         }
-        return rewardStreak;
+        return getNestedJson(profile, "rewardHighScore").getAsInt();
     }
 
-    public String getWins() {
+    public Integer getWins() {
         if (!profile.has("stats")) {
-            return "0";
+            return 0;
         }
-        Integer totalWins = 0;
+        int totalWins = 0;
 
         // Arcade
-        JsonObject arcade = (JsonObject) getNestedJson(profile, "stats", "Arcade");
-        List<String> gameKeysArcade = Arrays.asList(
-                "wins_party", "wins_soccer", "wins_mini_walls", "wins_party_2", "wins_farm_hunt",
-                "wins_ender", "wins_dayone", "wins_simon_says", "wins_oneinthequiver",
-                "seeker_wins_hide_and_seek", "hider_wins_hide_and_seek", "wins_hole_in_the_wall",
-                "sw_game_wins", "wins_zombies", "wins_hypixel_sports", "wins_draw_their_thing",
-                "wins_throw_out", "wins_santa_simulator", "wins_dragonwars2", "wins_easter_simulator",
-                "wins_scuba_simulator", "wins_halloween_simulator", "wins_grinch_simulator_v2",
-                "pixel_party.wins", "woolhunt_participated_wins", "dropper.wins"
+        JsonObject stats = (JsonObject) getNestedJson(profile, "stats");
+        List<String> winsKeys = Arrays.asList(
+                // Arcade
+                "Arcade.wins_party", "Arcade.wins_soccer", "Arcade.wins_mini_walls", "Arcade.wins_party_2", "Arcade.wins_farm_hunt",
+                "Arcade.wins_ender", "Arcade.wins_dayone", "Arcade.wins_simon_says", "Arcade.wins_oneinthequiver",
+                "Arcade.seeker_wins_hide_and_seek", "Arcade.hider_wins_hide_and_seek", "Arcade.wins_hole_in_the_wall",
+                "Arcade.sw_game_wins", "Arcade.wins_zombies", "Arcade.wins_hypixel_sports", "Arcade.wins_draw_their_thing",
+                "Arcade.wins_throw_out", "Arcade.wins_santa_simulator", "Arcade.wins_dragonwars2", "Arcade.wins_easter_simulator",
+                "Arcade.wins_scuba_simulator", "Arcade.wins_halloween_simulator", "Arcade.wins_grinch_simulator_v2",
+                "Arcade.pixel_party.wins", "Arcade.woolhunt_participated_wins", "Arcade.dropper.wins",
+
+                // Arena Brawl
+                "Arena.wins",
+
+                // Warlords
+                "Battleground.wins",
+
+                // Blitz SG TODO: Double check this
+                "HungerGames.wins", "HungerGames.wins_teams",
+
+                // Cops and Crims TODO: Double check this
+                "MCGO.game_wins", "MCGO.game_wins_deathmatch", "MCGO.game_wins_gungame",
+
+                // Paintball
+                "Paintball.wins",
+
+                // Quakecraft TODO: Double check this
+                "Quake.wins", "Quake.wins_teams", "Quake.wins_solo_tourney", "Quake.wins_tourney_quake_solo2_1",
+
+                // TNT Games TODO: Double check this
+                "TNTGames.wins", "TNTGames.wins_tourney_tnt_run_0", "TNTGames.wins_tourney_tnt_run_1",
+
+                // UHC TODO: Double check this
+                "UHC.wins", "UHC.wins_solo",
+
+                // VampireZ
+                "VampireZ.human_wins", "VampireZ.vampire_wins",
+
+                // Walls
+                "Walls.wins",
+
+                // Mega Walls
+                "Walls3.wins",
+
+                // Turbo Kart Racers TODO: Reduce to golds?
+                "GingerBread.gold_trophy", "GingerBread.tourney_gingerbread_solo_1_gold_trophy",
+
+                // SkyWars
+                "SkyWars.wins",
+
+                // Crazy Walls
+                "TrueCombat.wins",
+
+                // Smash Heroes
+                "SuperSmash.wins",
+
+                // Speed UHC
+                "SpeedUHC.wins",
+
+                // SkyClash
+                "SkyClash.wins",
+
+                // Bed Wars TODO: Double check this
+                "Bedwars.wins_bedwars",
+
+                // Murder Mystery
+                "MurderMystery.wins",
+
+                // Duels
+                "Duels.wins",
+
+                // Build Battle
+                "BuildBattle.wins",
+
+                // Wool Wars
+                "WoolGames.wool_wars.stats.wins"
+
         );
 
-        totalWins += gameKeysArcade.stream()
+        totalWins += winsKeys.stream()
                 .mapToInt(key -> {
                     try {
-                        return getNestedJson(arcade, key.split("\\.")).getAsInt();
+                        return getNestedJson(stats, key.split("\\.")).getAsInt();
                     } catch (Exception e) {
                         return 0;
                     }
                 })
                 .sum();
 
-        // Arena
-        try {
-            totalWins += getNestedJson(profile, "stats", "Arena", "wins").getAsInt();
-        } catch (Exception e) {
-            // do nothing
+        return totalWins;
+    }
+
+    public Integer getKills() {
+        if (!profile.has("stats")) {
+            return 0;
         }
+        int totalKills = 0;
 
-        // Battleground (Warlords)
-        try {
-            totalWins += getNestedJson(profile, "stats", "Battleground", "wins").getAsInt();
-        } catch (Exception e) {
-            // do nothing
-        }
+        JsonObject stats = (JsonObject) getNestedJson(profile, "stats");
+        List<String> killsKeys = Arrays.asList(
+                // Arcade
+                "Arcade.sw_kills", "Arcade.kills_dragonwars2", "Arcade.kills_throw_out", "Arcade.kills_oneinthequiver", // Is this right?
+                "Arcade.kills_mini_walls", "Arcade.final_kills_mini_walls", "Arcade.rpg_16_kills_party", "Arcade.hunter_kills_farm_hunt",
+                "Arcade.kills_farm_hunt", "Arcade.woolhunt_kills",
 
-        // HungerGames (Blitz)
+                // Arena Brawl
+                "Arena.kills_1v1", "Arena.kills_2v2", "Arena.kills_4v4",
 
+                // Warlords
+                "Battleground.kills",
 
+                // Blitz SG
+                "HungerGames.kills",
 
+                // Cops and Crims TODO: Check tournament kills
+                "MCGO.kills", "MCGO.kills_deathmatch", "MCGO.kills_gungame",
 
-        DecimalFormat df = new DecimalFormat("#,###");
-        return df.format(totalWins);
+                // Paintball
+                "Paintball.kills",
+
+                // Quakecraft TODO: Double check this
+                "Quake.kills", "Quake.kills_teams",
+
+                // TNT Games
+                "TNTGames.kills_capture", "TNTGames.kills_tntag", "TNTGames.kills_pvprun",
+
+                // UHC
+                "UHC.kills", "UHC.kills_solo",
+
+                // VampireZ
+                "VampireZ.human_kills", "VampireZ.vampire_kills",
+
+                // Walls
+                "Walls.kills",
+
+                // Mega Walls
+                "Walls3.kills",
+
+                // SkyWars
+                "SkyWars.kills",
+
+                // Crazy Walls
+                "TrueCombat.kills",
+
+                // Smash Heroes
+                "SuperSmash.kills",
+
+                // Speed UHC
+                "SpeedUHC.kills_insane", "SpeedUHC.kills_normal",
+
+                // SkyClash
+                "SkyClash.kills",
+
+                // Bed Wars
+                "Bedwars.kills_bedwars", "Bedwars.final_kills_bedwars",
+
+                // Murder Mystery
+                "MurderMystery.kills",
+
+                // Duels
+                "Duels.kills", "Duels.bridge_kills",
+
+                // Pit
+                "Pit.pit_stats_ptl.kills",
+
+                // Wool Wars
+                "WoolGames.wool_wars.stats.kills"
+
+                );
+        totalKills += killsKeys.stream()
+                .mapToInt(key -> {
+                    try {
+                        return getNestedJson(stats, key.split("\\.")).getAsInt();
+                    } catch (Exception e) {
+                        return 0;
+                    }
+                })
+                .sum();
+
+        return totalKills;
+
+    }
+
+    public JsonElement getAchievements() {
+        // return an object with profile["achievements"],profile["achievementsOneTime"]
+        JsonObject combined = new JsonObject();
+        combined.add("achievements", getNestedJson(profile, "achievements"));
+        combined.add("achievementsOneTime", getNestedJson(profile, "achievementsOneTime"));
+        return combined;
+    }
+
+    public Integer getLegacyAchievementPoints() {
+        return 0;
+
     }
 }
 
