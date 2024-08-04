ec32e3eee9144ddbffed99ad409a331e13a908e3
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-08-04 16:17:22 -0400

    InteractionId is now a class, return Message Data instead of just embed,

---
+++ src/main/java/me/stuffy/stuffybot/commands/InteractionId.java
@@ -0,0 +1,35 @@
+package me.stuffy.stuffybot.commands;
+
+import java.util.HashMap;
+
+public class InteractionId {
+    private final String command;
+    private final String userId;
+    private final HashMap<String, String> options;
+    public InteractionId(String componentId) {
+        String[] parts = componentId.split(":");
+
+        this.command = parts[0];
+        this.userId = parts[1];
+
+        String options = parts[2];
+        this.options = new HashMap<>();
+        String[] optionParts = options.split(",");
+        for (String option : optionParts) {
+            String[] optionParts2 = option.split("=");
+            this.options.put(optionParts2[0], optionParts2[1]);
+        }
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public HashMap<String, String> getOptions() {
+        return this.options;
+    }
+}
--- src/main/java/me/stuffy/stuffybot/utils/Interactions.java
+++ src/main/java/me/stuffy/stuffybot/utils/Interactions.java
@@ -1,31 +1,55 @@
 package me.stuffy.stuffybot.utils;
 
+import me.stuffy.stuffybot.commands.InteractionId;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
-import java.util.Map;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import static me.stuffy.stuffybot.utils.DiscordUtils.makeErrorEmbed;
+import static net.dv8tion.jda.api.interactions.components.buttons.Button.secondary;
 
 public class Interactions {
 
-    public static MessageEmbed getResponse(String interactionId) throws InteractionException {
-        String[] parts = interactionId.split(":");
-        String command = parts[0];
-        String userId = parts[1];
-        String options = parts[2];
+    public static MessageCreateData getResponse(String componentId) throws InteractionException {
+
+        InteractionId interactionId;
+
+        try {
+            interactionId = new InteractionId(componentId);
+        } catch (Exception e) {
+            throw new InteractionException(new String[]{
+                    "You can't fool me, this is gibberish",
+                    "This is not the interaction you are looking for",
+                    "*Someone* messed up the interactionId, and it wasn't me",
+                    "I'm sorry, Dave. I'm afraid I can't do that",
+                    "You can't fool me, that button doesn't exist",
+                    "So, we're just making up our own buttons now?",
+                    "How did you even get here?"});
+        }
+
+        String command = interactionId.getCommand();
+        String userId = interactionId.getUserId();
+        HashMap<String, String> options = interactionId.getOptions();
 
         // TODO: Error handling for invalid command
+//        switch (command) {
+//            case "pit":
+//                return null;
+//            default:
+//                throw new InteractionException("Invalid command, how did you even get here?");
+//        }
         // TODO: Error handling for invalid userId
 
 
-        Map<String, String> optionsMap = DiscordUtils.parseOptions(options);
-        if(optionsMap == null){
+        if(options == null){
             throw new InteractionException("No options provided, what am I supposed to do with this?");
         }
 
-        if(optionsMap.containsKey("ign")){
-            String ign = optionsMap.get("ign");
+        if(options.containsKey("ign")){
+            String ign = options.get("ign");
             if (ign.length() > 16){
                 throw new InteractionException("Invalid field `ign`, Usernames cannot be longer than 16 characters");
             }
@@ -36,6 +60,9 @@ public class Interactions {
         }
 
         MessageEmbed embed = makeErrorEmbed("Invalid interactionId", "Invalid interactionId");
-        return embed;
+        MessageCreateBuilder data = new MessageCreateBuilder()
+                .addEmbeds(embed)
+                .addActionRow(secondary("This is a button", "button"));
+        return data.build();
     }
 }
