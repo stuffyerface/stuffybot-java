70e1354a129a366727ece21d9125ec7c29e28e1f
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-08-29 01:53:13 -0400

    remove responses

--- src/main/java/me/stuffy/stuffybot/responses/BaseResponse.java
+++
@@ -1,9 +0,0 @@
-package me.stuffy.stuffybot.responses;
-
-import net.dv8tion.jda.api.entities.MessageEmbed;
-
-public class BaseResponse {
-    protected MessageEmbed getResponse(String action, String args) {
-        return null;
-    }
-}
