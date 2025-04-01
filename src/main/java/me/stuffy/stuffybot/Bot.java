396ea564bff3961620e2fd0c2451779560c195c4
Stuffy <mattd1800@gmail.com>
2025-03-31 22:27:34 -0400

    custom status

--- src/main/java/me/stuffy/stuffybot/Bot.java
+++ src/main/java/me/stuffy/stuffybot/Bot.java
@@ -45,7 +45,8 @@ public class Bot extends ListenerAdapter {
         String token = System.getenv("BOT_TOKEN");
         JDABuilder builder = JDABuilder.createDefault(token);
         builder.enableIntents(GatewayIntent.MESSAGE_CONTENT); // # TODO: Remove intents when possible
-        builder.setActivity(Activity.customStatus("almost ready..."));
+        String customStatus = Config.getCustomStatus();
+        builder.setActivity(Activity.customStatus(customStatus));
         builder.addEventListeners(this);
         JDA jda = builder.build().awaitReady();
         this.jda = jda;
