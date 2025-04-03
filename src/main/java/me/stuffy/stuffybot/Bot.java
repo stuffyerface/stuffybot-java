fef26ebd8be0b8a538b7f7d40c74e98fa74a242b
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2025-04-02 20:50:36 -0400

    remove message content intents

--- src/main/java/me/stuffy/stuffybot/Bot.java
+++ src/main/java/me/stuffy/stuffybot/Bot.java
@@ -44,7 +44,7 @@ public class Bot extends ListenerAdapter {
         // Get token from env variable
         String token = System.getenv("BOT_TOKEN");
         JDABuilder builder = JDABuilder.createDefault(token);
-        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT); // # TODO: Remove intents when possible
+//        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT); // # TODO: Remove intents when possible
         String customStatus = Config.getCustomStatus();
         builder.setActivity(Activity.customStatus(customStatus));
         builder.addEventListeners(this);
